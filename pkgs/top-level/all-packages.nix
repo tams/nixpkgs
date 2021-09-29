914a04c08a647be5fe9741742ec5320f63a5dd03
Jan Tojnar <jtojnar@gmail.com>
2021-09-29 16:01:00 +0200

    libsoup3: init at 3.0.1
    https://gitlab.gnome.org/GNOME/libsoup/-/blob/9be41160825b8238b482208b46bfe16b2f239ce3/NEWS

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17541,6 +17541,8 @@ with pkgs;
 
   libsoup = callPackage ../development/libraries/libsoup { };
 
+  libsoup3 = callPackage ../development/libraries/libsoup/3.x.nix { };
+
   libspectrum = callPackage ../development/libraries/libspectrum { };
 
   libspiro = callPackage ../development/libraries/libspiro {};
