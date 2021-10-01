1407c46966e68c8744a6b13622f9f2603027503a
Jan Tojnar <jtojnar@gmail.com>
2021-10-01 06:34:43 +0200

    fixup! sysprof: 3.40.1 → 3.42.0

--- pkgs/development/tools/profiling/sysprof/default.nix
+++ pkgs/development/tools/profiling/sysprof/default.nix
@@ -2,6 +2,7 @@
 , lib
 , desktop-file-utils
 , fetchurl
+, fetchpatch
 , gettext
 , glib
 , gtk3
@@ -29,6 +30,14 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "PBbgPv3+XT5xxNI5xndBrTf3LOiXHi9/rxaNvV6T6IY=";
   };
+
+  patches = [
+    # Fix missing unistd.h include.
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/sysprof/commit/b113c89af1de2f87589175795a197f6384852a78.patch";
+      sha256 = "3Q8d6IZYNJl/vbyzRgoRR2sdl4aRkbcKPeVjSSqxb98=";
+    })
+  ];
 
   nativeBuildInputs = [
     desktop-file-utils
